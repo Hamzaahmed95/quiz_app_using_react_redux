01be9a4088d7af1afbdf1ad45125287dc4586292
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2021-01-23 22:53:29 +0800

    added responsiveness

--- src/constants/dummyData.js
+++ src/constants/dummyData.js
@@ -3,108 +3,112 @@ export const data = {
   question: [
     {
       id: "1",
-      description: "This is first Question",
-      timer: 20.00,
+      description:
+        "حضرت ابوبکر صدیق رضی اللہ تعالی عنہ نے کتنے غلاموں کو آزاد کروایا",
+      timer: 20.0,
       answer: [
         {
           id: "1",
-          details: "This is answer1 of First Question",
+          details: "6",
           isTrue: false
         },
         {
           id: "2",
-          details: "This is answer2 of First Question",
-          isTrue: false
-        },
-        {
-          id: "3",
-          details: "This is answer3 of First Question",
+          details: "7",
           isTrue: true
         },
         {
+          id: "3",
+          details: "10",
+          isTrue: false
+        },
+        {
           id: "4",
-          details: "This is answer4 of First Question",
+          details: "4",
           isTrue: false
         }
       ]
     },
     {
       id: "2",
-      description: "This is second Question",
-      timer: 20.00,
+      description:
+        "وہ کونسی غزوہ تھی جس میں حضرت ابوبکر صدیق رضی اللہ تعالیٰ عنہٗ نے اپنے گھر کا تمام سامان اللہ کی راہ میں خرچ کیا",
+      timer: 20.0,
       answer: [
         {
           id: "1",
-          details: "This is answer1 of second Question",
+          details: "غزوہ حنین",
           isTrue: false
         },
         {
           id: "2",
-          details: "This is answer2 of second Question",
-          isTrue: true
+          details: "غزوہ خندق",
+          isTrue: false
         },
         {
           id: "3",
-          details: "This is answer3 of second Question",
+          details: "غزوہ خیبر",
           isTrue: false
         },
         {
           id: "4",
-          details: "This is answer4 of second Question",
-          isTrue: false
+          details: "غزوہ تبوک",
+          isTrue: true
         }
       ]
     },
     {
       id: "3",
-      description: "This is third Question",
-      timer: 20.00,
+      description:
+        "ایک صحابی  کی کفالت کے انکار پر اللہ رب العالمین نے قرآن میں حضرت ابوبکر  رضی اللہ عنہ کے لیے قرآن کی کونسی سورت سے آیت نازل کی",
+      timer: 20.0,
       answer: [
         {
           id: "1",
-          details: "This is answer1 of third question",
-          isTrue: true
+          details: "سورۃ احزاب",
+          isTrue: false
         },
         {
           id: "2",
-          details: "This is answer2 of third question",
+          details: "سورۃ توبہ",
           isTrue: false
         },
         {
           id: "3",
-          details: "This is answer3 of third question",
+          details: "سورۃ آل عمران",
           isTrue: false
         },
         {
           id: "4",
-          details: "This is answer4 of third question",
-          isTrue: false
+          details: "سورہ نور",
+          isTrue: true
         }
       ]
     },
     {
       id: "4",
-      description: "This is fourth Question",
-      timer: 20.00,
+      description:
+        "ان میں وہ کون سے صحابی ہیں  جو حضرت ابوبکر صدیق رضی اللہ تعالی عنہ کی کوششوں سے ایمان لائے",
+      timer: 20.0,
       answer: [
         {
           id: "1",
-          details: "This is answer1 of fourth question",
+          details: "حضرت ابو عبیدہ بن جراح رضی اللہ عنہ",
           isTrue: false
         },
         {
           id: "2",
-          details: "This is answer2 of fourth question",
+          details: "حضرت عبدالرحمٰن بن عوف رضی اللہ عنہ",
           isTrue: false
         },
         {
           id: "3",
-          details: "This is answer3 of fourth question",
+          details: "حضرت عثمان بن عفان رضی اللہ عنہ",
           isTrue: false
         },
         {
           id: "4",
-          details: "This is answer4 of fourth question",
+          details: "تینوں",
           isTrue: true
         }
       ]
@@ -112,27 +116,112 @@ export const data = {
 
     {
       id: "5",
-      description: "This is first Question",
-      timer: 20.00,
+      description:
+        "ایک شخص نے جب حضرت ابوبکر صدیق رضی اللہ تعالی عنہ کو جادو ٹونہ کی کمائی سے کچھ کھلایا  تو معلوم ہونے پر حضرت ابوبکر رضی اللہ عنہٗ کا کیا ردعمل تھا",
+      timer: 20.0,
       answer: [
         {
           id: "1",
-          details: "This is answer1",
+          details: "استغفار کی",
           isTrue: false
         },
         {
           id: "2",
-          details: "This is answer2",
+          details: "انگلی منہ میں ڈال کر قے کیا",
+          isTrue: true
+        },
+        {
+          id: "3",
+          details: "اس شخص سے قطع تعلقی کی",
+          isTrue: false
+        },
+        {
+          id: "4",
+          details: "کوئی رد عمل نہیں کیا",
+          isTrue: false
+        }
+      ]
+    },
+    {
+      id: "6",
+      description:
+        "وہ کون تھا  جس نے آپ صلی اللہ علیہ وآلہ وسلم  کی گردن مبارک  پر  رسی کس دی تھی  حضرت ابوبکر صدیق رضی اللہ تعالی عنہ آپ  صلی اللہ علیہ وسلم  کے دفاع میں اس سے لڑے",
+      timer: 20.0,
+      answer: [
+        {
+          id: "1",
+          details: "ابوجہل",
+          isTrue: false
+        },
+        {
+          id: "2",
+          details: "امیہ بن خلف",
           isTrue: false
         },
         {
           id: "3",
-          details: "This is answer3",
+          details: "عقبہ بن ابی معیط",
           isTrue: true
         },
         {
           id: "4",
-          details: "This is answer4",
+          details: "ابو لہب",
+          isTrue: false
+        }
+      ]
+    },
+    {
+      id: "7",
+      description:
+        "غار ثور میں  آخری سوراخ کو حضرت ابوبکر صدیق رضی اللہ تعالی عنہ نے کس چیز سے ڈھانپا",
+      timer: 20.0,
+      answer: [
+        {
+          id: "1",
+          details: "اپنے ہاتھ کے انگوٹھے سے",
+          isTrue: false
+        },
+        {
+          id: "2",
+          details: "اپنی  ایڑھی سے ",
+          isTrue: true
+        },
+        {
+          id: "3",
+          details: "اپنے پاؤں کے انگوٹھے سے",
+          isTrue: false
+        },
+        {
+          id: "4",
+          details: "ٹخنے سے",
+          isTrue: false
+        }
+      ]
+    },
+    {
+      id: "8",
+      description:
+        "ایک ہی دن میں چار عمل( روزہ,  نماز جنازہ تدفین,  مسکین کو کھانا کھلانا) کرنے پر رسول صلی اللہ علیہ وسلم نے حضرت ابوبکر صدیق رضی اللہ عنہ کو جنت کی بشارت دی.  چوتھا   عمل کونسا ہے",
+      timer: 20.0,
+      answer: [
+        {
+          id: "1",
+          details: "تہجد",
+          isTrue: false
+        },
+        {
+          id: "2",
+          details: "مریض کی عیادت ",
+          isTrue: true
+        },
+        {
+          id: "3",
+          details: "صدقہ",
+          isTrue: false
+        },
+        {
+          id: "4",
+          details: "پڑوسی کے ساتھ حسن سلوک",
           isTrue: false
         }
       ]
