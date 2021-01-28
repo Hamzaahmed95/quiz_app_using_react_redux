28f848b58ea1bd0fa5717d7fea0b7bffec66b671
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2021-01-28 18:51:27 +0800

    update changes in data file

--- src/components/result/index.js
+++ src/components/result/index.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
+import { Button } from "@material-ui/core";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
@@ -10,22 +11,8 @@ import Paper from "@material-ui/core/Paper";
 import firebase from "firebase";
 import "./index.css";
 
-const useStyles = makeStyles({});
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Hamza", 27),
-  createData("Umer", 22),
-  createData("Fayez", 17),
-  createData("People", 12),
-  createData("Poepl1", 11)
-];
-
 const Result = () => {
-  const classes = useStyles();
+  const [resultShown, isResultShown] = useState(false);
   const [data, setData] = useState([]);
   let datas = [];
   useEffect(() => {
@@ -42,7 +29,7 @@ const Result = () => {
           a.totalCorrectAnswers < b.totalCorrectAnswers
             ? 1
             : a.totalCorrectAnswers === b.totalCorrectAnswers
-            ? a.score < b.score
+            ? a.score > b.score
               ? 1
               : -1
             : -1
@@ -64,38 +51,48 @@ const Result = () => {
       >
         Results
       </span>
-      <TableContainer className="container">
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <b className="color">Name</b>
-              </TableCell>
-              <TableCell align="right">
-                <b className="color">Correct Answers</b>
-              </TableCell>
-              <TableCell align="right">
-                <b className="color">Score</b>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map(result => (
-              <TableRow key={result.id}>
-                <TableCell className="color" component="th" scope="row">
-                  <p className="color">{result.name}</p>
+      {resultShown && (
+        <TableContainer className="container">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <b className="color">Name</b>
                 </TableCell>
-                <TableCell className="color" align="right">
-                  <p className="color">{result.totalCorrectAnswers}</p>
+                <TableCell align="right">
+                  <b className="color">Correct Answers</b>
                 </TableCell>
-                <TableCell className="color" align="right">
-                  <p className="color">{result.score}</p>
+                <TableCell align="right">
+                  <b className="color">Score</b>
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {data.map(result => (
+                <TableRow key={result.id}>
+                  <TableCell className="color" component="th" scope="row">
+                    <p className="color">{result.name}</p>
+                  </TableCell>
+                  <TableCell className="color" align="right">
+                    <p className="color">{result.totalCorrectAnswers}</p>
+                  </TableCell>
+                  <TableCell className="color" align="right">
+                    <p className="color">{result.score}</p>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+      <br />
+      <button
+        onClick={() => isResultShown(!resultShown)}
+        style={{ marginTop: "10%" }}
+        color="white"
+      >
+        {!resultShown ? "Show result" : "Hide result"}
+      </button>
     </div>
   );
 };
